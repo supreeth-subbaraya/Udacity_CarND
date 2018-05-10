c8afee81816aaa5a95b5e3ad6ec304baaa5cd55f
Supreeth Subbaraya <supreeth324@gmail.com>
2018-05-09 23:11:26 -0400

    New values for N and dt

--- CarND-MPC-Project/src/MPC.cpp
+++ CarND-MPC-Project/src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 10;
-double dt = 0.1;
+size_t N = 15;
+double dt = 0.05;
 
 size_t x_start = 0;
 size_t y_start = x_start + N;
